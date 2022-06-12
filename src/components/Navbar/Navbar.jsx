fd36b3405f6c1b1f5af25047b7d88d7e82e9b1c8
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-06-12 13:26:09 +0200

    code cleaned

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import "./Navbar.css"
-import MenuImg from "../../assets/menu.png"
+import React from "react";
+import "./Navbar.css";
+import MenuImg from "../../assets/menu.png";
 
-const SobreGoogleLink = "https://about.google/?fg=1&utm_source=google-ES&utm_medium=referral&utm_campaign=hp-header"
-const TiendaGoogleLink = "https://store.google.com/ES?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=es-ES&pli=1"
-const GmailLink ="https://mail.google.com/mail/&ogbl"
-const ImagenesLink = "https://www.google.es/imghp?hl=es&ogbl"
-
+const SobreGoogleLink =
+  "https://about.google/?fg=1&utm_source=google-ES&utm_medium=referral&utm_campaign=hp-header";
+const TiendaGoogleLink =
+  "https://store.google.com/ES?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=es-ES&pli=1";
+const GmailLink = "https://mail.google.com/mail/&ogbl";
+const ImagenesLink = "https://www.google.es/imghp?hl=es&ogbl";
 
 const Navbar = (navBar) => {
   return (
@@ -18,11 +19,11 @@ const Navbar = (navBar) => {
       <div className="right-box">
         <a href={GmailLink}> {navBar.items.navBar2.item1}</a>
         <a href={ImagenesLink}> {navBar.items.navBar2.item2}</a>
-        <img src={MenuImg}/>
+        <img src={MenuImg} />
         <button className="LoginButton">{navBar.items.navBar2.item3}</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
